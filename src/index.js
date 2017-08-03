6f9824713e4089fe5eaf389f73f538aa76031419
Jhonson <jhonson.tian@yahoo.com>
2017-08-03 17:03:31 +0700

    little progress

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import registerServiceWorker from './registerServiceWorker';
 class Square extends React.Component {
   render() {
     return (
-      <button className="btn-square">X</button>
+      <button className="btn-square" onClick={this.props.onClick}>{this.props.val}</button>
     )
   }
 }
 class BoardGame extends React.Component {
   renderSquare(i) {
     return (
-      <Square
+      <Square 
+        val={this.props.val[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
@@ -22,19 +24,19 @@ class BoardGame extends React.Component {
     return (
       <div className="board-game">
         <div className="row-square">
+          {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-          {this.renderSquare(3)}
         </div>
           <div className="row-square">
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          {this.renderSquare(6)}
         </div>
           <div className="row-square">
+          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
         </div>   
       </div>
     )
@@ -76,23 +78,89 @@ class OneOrTwoPlayer extends React.Component {
 }
 class ChooseChar extends React.Component {
   renderButton(i) {
-    let temp = i === 1 ? "X" : "O";
-    return (
-      <PlayerButton
-        className ="btn-char"
-        text={temp} 
-        onClick={() => this.props.onClick(i)}
-      />
-    )
+    if (i < 3) {
+      let temp = i === 1 ? "X" : "O";
+      return (
+        <PlayerButton
+          className ="btn-char"
+          text={temp} 
+          onClick={() => this.props.onClick(i)}
+        />
+      )
+    } else {//back button
+      return (
+        <PlayerButton
+          className ="btn-back"
+          text= {String.fromCharCode(10594) + "Back"}
+          onClick={() => this.props.onClick(i)}
+        />
+      )
+    }
   }
   render() {
+    let question = '';
+    if (this.props.numPlayer === 1) {
+      question = 'Would you like to be X or O?';
+    } else {
+      question = 'Player 1 : Would you like X or O?'
+    }
     return (
-      <div className="intro-choose">
-        <p>Would you like to be X or O?</p>
-        <div className="intro-choose-btn">
+      <div className="intro">
+        <p>{question}</p>
+        <div className="choose-btn">
           {this.renderButton(1)}
           {this.renderButton(2)}          
         </div>
+          {this.renderButton(3)}          
+      </div>
+    )
+  }
+}
+class Status extends React.Component {
+  render() {
+    return (
+      <span>{this.props.text}</span>
+    )
+  }
+}
+class Reset extends React.Component {
+  render() {
+    return <button className="reset-btn" onClick={this.props.onClick}>Reset All</button>
+  }
+}
+class PlayerStats extends React.Component {
+  renderStatus(i) {
+    let temp = '';
+    switch (i) {
+      case 1:
+        temp = this.props.win1;
+        break;
+      case 2:
+        temp = "player 1";
+        break;
+      case 3:
+        temp = this.props.win2;
+        break;
+      case 4:
+        temp = this.props.numPlayer === 1 ? "computer" : "player 2";
+    }
+    return <Status text={temp}/>
+  }
+  render() {
+    return (
+      <div>
+        <div className="player1-stats">
+          {this.renderStatus(1)}
+          <br />
+          {this.renderStatus(2)} 
+        </div>
+  
+        <div className="player2-stats">
+          {this.renderStatus(3)}
+          <br />
+          {this.renderStatus(4)}
+        </div>
+        <Reset onClick={this.props.onClick}/>
       </div>
     )
   }
@@ -104,35 +172,102 @@ class Board extends React.Component {
       page: 1,
       numPlayer: 0,
       player1: '',
-      player2: ''
+      player2: '',
+      win1: 0,
+      win2: 0,
+      squares: Array(9).fill(null),
+      start: true,
+      turn: ''
     }
     this.handleClickPlayer = this.handleClickPlayer.bind(this);
     this.handleClickChar = this.handleClickChar.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+    this.handleClickSquare = this.handleClickSquare.bind(this);
+  }
+  handleResetClick() {
+    this.setState({
+      win1: 0,
+      win2: 0,
+      squares: Array(9).fill(null),
+      start: true
+    });
   }
   handleClickPlayer(i) {
     this.setState({
-      page: 2,
-      numPlayer: i
+      numPlayer: i,
+      page: 2
     });
   }
   handleClickChar(i) {
-    // let char = i.target.value;
-    console.log(i.value);
-    // this.setState({
-    //   page: 3,
-    //   player1: char,
-    //   player2: char === 'X' ? 'O' : 'X'
-    // });
+    if (i < 3) {
+      this.setState({
+        page: 3,
+        player1: i === 1 ? 'X' : 'O',
+        player2: i === 2 ? 'X' : 'O'
+      });
+    } else { //back button
+      this.setState({page: 1});
+    }
+  }
+  handleClickSquare(i) {
+    console.log(this.state.squares[i]);
+    let squares = this.state.squares.slice();
+    squares[i] = 'x';
+    this.setState({squares});
+    // if (this.state.turn === '1') {
+    //   squares[i] = this.state.player1;
+    //   this.setState({
+    //     turn: '2',
+    //     squares: squares
+    //   });
+    // } else {
+    //   squares[i] = this.state.player2;
+    //   this.setState({
+    //     turn: '1',
+    //     squares: squares
+    //   });
+    // }
   }
   render() {
     let myComponent = undefined;
+    let myPlayer = undefined;
     if (this.state.page === 1) {
       myComponent = <OneOrTwoPlayer onClick={(i) => this.handleClickPlayer(i)} /> ;
+      myPlayer = undefined;
     } else if (this.state.page === 2) {
-      myComponent = <ChooseChar onClick={(i) => this.handleClickChar(i)}/>;
+      myComponent = <ChooseChar onClick={(i) => this.handleClickChar(i)} numPlayer={this.state.numPlayer}/>;
+      myPlayer = undefined;
+    } else {
+      myComponent = <BoardGame val={this.state.squares} onClick={(i) => this.handleClickSquare(i)} />;
+      myPlayer = (<PlayerStats 
+        win1={this.state.win1} 
+        win2={this.state.win2}
+        numPlayer={this.state.numPlayer}
+        onClick={this.handleResetClick}
+      />);
+    // if (this.state.start) {
+    //   //random first player
+    //   let turn = Math.floor((Math.random() * 2) + 1);
+    //   if (turn === 1) {
+    //     this.setState({turn: '1', start: false});
+    //   } else {
+    //     if (this.state.numPlayer === 2) {
+    //       this.setState({turn: '2', start: false});
+    //     } else {
+    //       this.setState({turn: 'c', start: false});
+    //     }
+    //   }
+    // } else { //game logic
+    //   if (this.state.turn === 'c') {
+    //     console.log('computer');
+    //   }
+    // }
     }
     return (
       <div className="outer-board">
+        <div className="status-container">
+          {myPlayer}
+        </div>
         <div className="inner-board">
           {myComponent}
         </div>
